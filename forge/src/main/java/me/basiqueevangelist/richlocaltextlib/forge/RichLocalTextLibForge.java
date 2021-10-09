a23f3bb15f70e4e4ad4ef0a7b19f7a19f62f31aa
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-10-10 00:21:26 +0300

    1.17™

--- forge/src/main/java/me/basiqueevangelist/richlocaltextlib/forge/RichLocalTextLibForge.java
+++ forge/src/main/java/me/basiqueevangelist/richlocaltextlib/forge/RichLocalTextLibForge.java
@@ -1,11 +1,10 @@
 package me.basiqueevangelist.richlocaltextlib.forge;
 
 import me.basiqueevangelist.richlocaltextlib.RichLocalTextLib;
-import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
-import org.apache.commons.lang3.tuple.Pair;
+import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
 
 @Mod(RichLocalTextLib.MOD_ID)
 public class RichLocalTextLibForge {
@@ -13,6 +12,9 @@ public class RichLocalTextLibForge {
         // Submit our event bus to let architectury register our content on the right time
         RichLocalTextLib.init();
 
-        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(
+            () -> FMLNetworkConstants.IGNORESERVERONLY,
+            (a, b) -> true
+        ));
     }
 }
